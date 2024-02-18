41ac56b98b956dd1a3d54847c0d743815c4e8903
Andrey Sobolev <a.sobolev@agora.ru>
2024-02-18 23:55:58 +0300

    Включил пагинацию в настройках, обновил файл со схемой API

--- task3/server/server/server/settings.py
+++ task3/server/server/server/settings.py
@@ -136,7 +136,8 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 SPECTACULAR_SETTINGS = {
