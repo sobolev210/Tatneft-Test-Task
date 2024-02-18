235f48b391a07df93196a37df9ae1f70f6d88e81
Andrey Sobolev <a.sobolev@agora.ru>
2024-02-18 23:10:34 +0300

    Добавил информацию по запуску Сервера-посредника в Readme, добавил автозапуск выполняющихся по расписанию задач при запуске celery

--- task3/server/server/server/celery.py
+++ task3/server/server/server/celery.py
@@ -6,3 +6,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 app = Celery("server")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+
+@worker_ready.connect
+def at_start(sender, **k):
+    with sender.app.connection() as conn:
+        sender.app.send_task('apps.core.tasks.load_data_from_source_1', connection=conn)
+        sender.app.send_task('apps.core.tasks.load_data_from_source_2', connection=conn, countdown=20)
+
